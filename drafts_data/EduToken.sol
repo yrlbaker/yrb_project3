6cd858c8f61c05c3311575aae1a11bb09aed4fab
yrlbaker <curativefinance@gmail.com>
2021-08-22 18:53:14 -0500

    Added code and notes to EduToken.sol

--- drafts_data/EduToken.sol
+++ drafts_data/EduToken.sol
@@ -1,23 +1,25 @@
 pragma solidity ^0.5.0;
 
 import "github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/math/SafeMath.sol";
-
+// Using SafeMath by OpenZeppelin, to make secure and strong Solidity contracts, 
+// and also use for unsigned integers.
+// The public token symbol is EDU, the exchange rate is 1 wei.
 contract EduToken {
     using SafeMath for uint;
-        // showing EDU token symbol
     address payable owner = msg.sender;
     string public symbol = "EDU";
     uint public exchange_rate = 1;
-
+    
+    // Balances have set of unsigned integer values mapped by the data type address.
     mapping(address => uint) balances;
 
-        //This is where you can see the balance of the account requested.
+    //This is where you can see the balance of the account requested.
     function balance() public view returns(uint) {
         return balances[msg.sender];
     }
 
-        // This takes the balance from the original owner of all tokens to 
-        // a new account that is requesting tokens.
+    // Transfer a certain amount of tokens from the balance of the original owner 
+    // to a new account that is requesting tokens.
     function transfer(address recipient, uint value) public {
         balances[msg.sender] = balances[msg.sender].sub(value);
         balances[recipient] = balances[recipient].add(value);
@@ -29,6 +31,8 @@ contract EduToken {
         owner.transfer(msg.value);
     }
 
+    // A request to mint the token can only come from the address payable owner.
+    // Otherwise, the permission is denied. 
     function mint(address recipient, uint value) public {
         require(msg.sender == owner, "You do not have permission to mint tokens!");
         balances[recipient] = balances[recipient].add(value);
